a1c7180b6fd3c34e29849ce6413b5f62b9f926b4
Brian Daley <brian@uconn.edu>
2026-06-09 15:34:41 -0400

    proxy shit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust all proxies (standard for dynamic container environments like OpenShift)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
